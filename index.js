f1999b9962cd2873fc70ff345f80499afdb11290
Michael Thomas (malinka) <malinka@entropy-development.com>
2017-05-12 18:24:27 -0400

    use locally written forward app

--- index.js
+++ index.js
@@ -6,47 +6,46 @@
 
 const koa = require('koa');
 const logger = require('koa-logger');
-const forward = require('koa-forward-request');
 const route = require('koa-route');
 const http = require('http');
+const body = require('koa-bodyparser');
+
+const forward = require('koa-forward');
 
 const config = require('config')(require('./config.js'));
 
 const app = new koa();
 
-forward(app, {
-	debug: (process.env.NODE_ENV !== 'production')	// 2017-04-23 AMR TODO: improve output format, or replace completely
-});
-
-const index = async (ctx, next) => {
-	console.log('asdf2');
-	console.log(ctx.forwards);
-
-	await next();
-
-	ctx.forward(ctx.forwards['static'].forward + '/index.html');
-};
-
 app.use(logger());
-app.use(async (ctx, next) => {
-	var forwarded = false;
+app.use(body());
+app.use(forward());
 
+app.use(async (ctx, next) => {
 	for(const f in ctx.forwards) {
 		if(ctx.request.url.startsWith(ctx.forwards[f].path)) {
-			forwarded = true;
-			ctx.forward(ctx.forwards[f].forward + ctx.request.url.replace(ctx.forwards[f].path, ""));
+			return await ctx.forward(ctx.forwards[f].forward + ctx.request.url.replace(ctx.forwards[f].path, ""));
 		}
 	}
 
-	if(!forwarded)
-		await next();
+	await next();
 });
-app.use(route.get('/favicon.ico', async (ctx) => {
-	ctx.throw(404);
+app.use(route.post('/asdf', async (ctx, next) => {
+	await next();
+
+	console.log(ctx.request);
+	console.log(ctx.request.rawBody);
+	console.log(ctx.request.body);
 }));
-app.use(async (ctx) => {
-	ctx.forward(ctx.forwards['static'].forward + 'default.html');
-});
+app.use(route.get('/favicon.ico', async (ctx, next) => {
+	await next();
+
+	return ctx.throw(404);
+}));
+app.use(route.get('/', async (ctx, next) => {
+	await next();
+
+	return await ctx.forward(ctx.forwards['static'].forward + 'default.html');
+}));
 
 app.listen(config.port);
 
